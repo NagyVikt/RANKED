f1861d52b96ab68e17ac4e9cba3b160c3d35b99e
imdeadpool1 <nagy.viktordp@gmail.com>
2024-09-06 08:47:47 +0200

    push

--- SK/u9.py
+++ SK/u9.py
@@ -160,7 +160,7 @@ def create_or_update_product_in_woocommerce(product_data):
             return None
 
 # Load the JSON file
-with open('pletiva_new.json', 'r', encoding='utf-8') as file:
+with open('loziska-gufera-klinove-remene-a-prislusenstvo.json', 'r', encoding='utf-8') as file:
     products = json.load(file)
 
 # Directory to save downloaded images
