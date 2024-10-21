b39aede21157bb2d65eae31fa59505849a8938f6
imdeadpool1 <nagy.viktordp@gmail.com>
2024-10-21 10:51:28 +0200

    new

--- SK/DEVELOPMENT/lanove-prislusenstvo-a-nerezovy-program.py
+++ SK/DEVELOPMENT/lanove-prislusenstvo-a-nerezovy-program.py
@@ -231,7 +231,7 @@ def create_main_category_json(category_data, output_file):
 
 def main():
     # Define the main category data
-    main_category_url = "https://www.svx.sk/naradie/"
+    main_category_url = "https://www.svx.sk/lanove-prislusenstvo-a-nerezovy-program/"
 
     # Start scraping from the main category
     print("Starting to scrape categories and subcategories...")
